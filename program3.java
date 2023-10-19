08061f7e07fc0f7ccc5413007817223d67b00c24
ne0psych <bharathcs46@protonmail.com>
2023-10-19 17:51:56 +0530

    program3  if else statement

---
+++ program3.java
@@ -0,0 +1,18 @@
+public class program3
+{
+    public static void main(String args[])
+    {
+        int i = 10;
+        int j = 20;                                                 //declaring variables
+
+        if(i<j)                                                     //condition
+        {
+            System.out.println("The greater number is " + i);       //if True
+        }
+        else
+        {
+            System.out.println("The greater number is " + j);       //if Flase
+        }
+    }
+
+}
