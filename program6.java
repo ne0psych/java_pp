d2c14c847d6e51e50e1f822c88b6a30be0d260ac
ne0psych <bharathcs46@protonmail.com>
2023-10-19 18:00:17 +0530

    While loop

---
+++ program6.java
@@ -0,0 +1,16 @@
+public class program6 
+{
+    public static void main(String args[])
+    {
+            int i=1;                                                //intialization
+
+            while(i<=4)                                             //condition
+            {
+                System.out.println("The Value of i is " + i);
+                i++;                                                //incrementation
+            }
+            
+            System.out.println("The value of i after the condition is false " + i);
+    }
+    
+}
