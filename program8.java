c8ca6ea71248643eed20474241fcd933a3c2639e
ne0psych <bharathcs46@protonmail.com>
2023-10-19 18:01:04 +0530

    For loop

---
+++ program8.java
@@ -0,0 +1,11 @@
+public class program8 
+{
+    public static void main(String args[])
+    {
+            for( int i=0; i<4;i++)
+            {
+                    System.out.println("The value is " + i);
+            }        
+        
+    }
+}
